8baf946058c4f028a85a32cb41f69daf0ff06cad
Relingga Aditya <126789510+relinggaa@users.noreply.github.com>
2026-05-20 00:40:56 +0700

    test :  pwa on bidan

--- app/page.tsx
+++ app/page.tsx
@@ -79,11 +79,11 @@ export default function Home() {
           </button>
           <a
             className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs"
-            target="_blank"
+            href="/bidan/home"
+
             rel="noopener noreferrer"
           >
-            Documentation
+            Bidan
           </a>
         </div>
       </main>
