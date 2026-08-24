db1d89e73d6fc3165ce3b4c31f094bad93d02e06
relinggaa <linggayorfel@gmail.com>
2026-08-24 18:57:20 +0700

    feat : create chart grafik pertumbuhan

--- hooks/query/nutrition/useManageNutritionRecords.ts
+++ hooks/query/nutrition/useManageNutritionRecords.ts
@@ -1,8 +1,22 @@
 "use client";
 
-import { useMutation, useQueryClient } from "@tanstack/react-query";
-import { createNutritionRecord } from "@/service/nutrition/nutritionRecordService";
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
+import {
+  createNutritionRecord,
+  fetchNutritionRecordsByChild,
+  FetchNutritionRecordsParams,
+} from "@/service/nutrition/nutritionRecordService";
 import { CreateNutritionRecordPayload } from "@/interfaces/nutrition";
+
+
+export function useGetNutritionRecordsByChild(params: FetchNutritionRecordsParams) {
+  return useQuery({
+    queryKey: ["nutrition-records-child", params],
+    queryFn: () => fetchNutritionRecordsByChild(params),
+    enabled: !!params.children_id,
+    staleTime: 5000,
+  });
+}
 
 export function useCreateNutritionRecord() {
   const queryClient = useQueryClient();
@@ -12,6 +26,7 @@ export function useCreateNutritionRecord() {
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ["child"] });
       queryClient.invalidateQueries({ queryKey: ["children"] });
+      queryClient.invalidateQueries({ queryKey: ["nutrition-records-child"] });
     },
   });
 }
